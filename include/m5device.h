e77246e25b65fb86807d7f102ea40c87b5d60820
jun fujita <716663+jfcamel@users.noreply.github.com>
2022-07-27 22:35:13 +0900

    Added common header

---
+++ include/m5device.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdbool.h>
+
+#include "sdkconfig.h"
+
+#include "esp_err.h"
+
+#if defined M5DEVICE_STICKC_PLUS
+#define M5DEVICE_PORTA_SDA      21
+#define M5DEVICE_PORTA_SCL      22
+#elif defined M5DEVICE_STACK_CORE2
+//...
+#endif // M5DEVICE_TYPE
+
+extern bool m5device_is_initialized;
+
+esp_err_t m5device_init(void);
+esp_err_t m5device_deinit(void);
+
+esp_err_t m5device_i2c_init(void);
+esp_err_t m5device_i2c_deinit(void);
+
+bool m5device_i2c_is_initialized(void);
